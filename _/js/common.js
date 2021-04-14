62b416e74b953ebc8e672e2891a01ff2e8ff48d9
CheAle14 <thedark1513@gmail.com>
2021-04-14 21:39:15 +0100

    Rules changable

--- _/js/common.js
+++ _/js/common.js
@@ -5,3 +5,21 @@ function openNav() {
 function closeNav() {
     document.getElementById("docSidebar").style.width = "0";
 }
+
+/**
+ * Gets a URL to the proper server.
+ * @param {string} pathAndQuery 
+ * @param {boolean} httpOrWss  True if HTTP(s), false if WS(s)
+ * @param {int} localPort The port used for the debug local connection
+ */
+function getUrl(pathAndQuery, httpOrWss = true, localPort = 8887) {
+    var prot = httpOrWss ? "http" : "ws";
+    var domain = "ml-api.uk.ms";
+    if(document.location.protocol === "http:") {
+        domain = "localhost:" + localPort
+    } else {
+
+        prot += "s";
+    }
+    return prot + "://" + domain + pathAndQuery;
+}
